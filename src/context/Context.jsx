0161d1bc790272812c86bb5f9275e59e7a90955f
rdogan7106 <rdogan7106@gmail.com>
2024-02-27 09:39:14 +0100

    context created

---
+++ src/context/Context.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [userList,setUserList]  = useState(null)
+  const [auctionsList, setAuctionsList] = useState(null)
+
+  const login = async (username, password) => {
+    const response = await fetch("http://localhost:3001/users");
+    const users = await response.json();
+    const auctionResponse = await fetch("http://localhost:3001/auctions");
+    const auctions = await auctionResponse.json();
+    setAuctionsList(auctions)
+    setUserList([users])
+    const user = users.find(
+      (user) => user.username === username && user.password === password
+    );
+    if (user) {
+        setUser(user); 
+      return true;
+    } else {
+      return false;
+    }
+ 
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+  
+  return (
+    <AuthContext.Provider value={{ user, login, logout, userList,setUserList, auctionsList ,setAuctionsList}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
