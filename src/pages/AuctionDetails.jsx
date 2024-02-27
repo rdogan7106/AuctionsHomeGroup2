80f405fbbee089dd25f5bbb20792d79362faeca8
Barileva <veton@live.se>
2024-02-27 13:23:46 +0100

    update

--- src/pages/AuctionDetails.jsx
+++ src/pages/AuctionDetails.jsx
@@ -17,7 +17,14 @@ function AuctionDetails({ auctionsList }){
                 <p>{auction.itemDetails.title}</p>
                 <p>{auction.itemDetails.description}</p>
                 <p>{auction.startTime}</p>
-                <p>{auction.endTime}</p>
+            <p>{auction.endTime}</p>
+            
+            <p>{auction.id}</p>
+            <p>{auction.sellerId}</p>
+            <p>{auction.itemDetails.title}</p>
+            <p>{auction.itemDetails.description}</p>
+            <p>{auction.startTime}</p>
+            <p>{auction.endTime}</p>
 
             </div>
         </div>
