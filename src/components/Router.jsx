6a57aec01b7a99257b2c84a9859b5c35defa347c
rdogan7106 <rdogan7106@gmail.com>
2024-02-27 09:44:56 +0100

    router created

---
+++ src/components/Router.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Appbar from "./Appbar.jsx";
+import HomePage from "../pages/Home.jsx";
+import Loginpage from "../pages/Loginpage.jsx";
+import Dashboard from "../pages/Dashboard.jsx";
+import AuctionsOnUserPage from "../pages/AuctionsOnUserPage.jsx";
+import AuctionDetails from "../pages/AuctionDetails.jsx";
+import { useAuth } from "../context/Context.jsx";
+
+function Router() {
+  const [user, setUser] = useState(null);
+  const { auctionsList } = useAuth();
+  const login = (user) => {
+    setUser(user);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <>
+      <Appbar user={user} logout={logout} />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/auctions" element={<AuctionsOnUserPage />}></Route>
+        <Route exact 
+          path="/auctions/:auctionId"
+          element={<AuctionDetails auctionsList={auctionsList} />}
+        />
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/login" element={<Loginpage login={login} />} />
+      </Routes>
+    </>
+  );
+}
+
+export default Router;
