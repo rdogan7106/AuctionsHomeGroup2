36aa3988f21efb288639f5803e94e3c6b78a3662
Barileva <veton@live.se>
2024-02-29 15:29:53 +0100

    added css-style with Bootstrap

--- src/pages/AuctionDetails.jsx
+++ src/pages/AuctionDetails.jsx
@@ -3,27 +3,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-function AuctionDetails({ auctionsList }){
-    let { auctionId } = useParams();
-    let auction = auctionsList.find(auction => auction.id.toString() === auctionId);
-    return (
-      <div className='d-flex p-5'>
-        <div className='w-50' id='item-details'>
-            <div className='d-flex justify-content-center' >
-            <img src={auction.itemDetails.image} className='w-50' alt=""  /></div>
-            <div id='info'>
-                <p>{auction.id}</p>
-                <p>{auction.sellerId}</p>
-                <p>{auction.itemDetails.title}</p>
-                <p>{auction.itemDetails.description}</p>
-                <p>{auction.startTime}</p>
-                <p>{auction.endTime}</p>
+function AuctionDetails({ auctionsList }) {
+  let { auctionId } = useParams();
+  let auction = auctionsList.find(auction => auction.id.toString() === auctionId);
+  return (
+    <div className='d-flex p-5'>
+      <div className='w-50' id='item-details'>
+        <img src={auction.itemDetails.image} className='img-fluid' alt="" style={{ maxWidth: '50%', height: '50%' }} />
 
-            </div>
+        <div id='info' className='text-left'>
+          <p className='mb-2'><strong>ID:</strong> {auction.id}</p>
+          <p className='mb-2'><strong>Säljarens ID:</strong> {auction.sellerId}</p>
+          <p className='mb-2'><strong>Titel:</strong> {auction.itemDetails.title}</p>
+          <p className='mb-2'><strong>Beskrivning:</strong> {auction.itemDetails.description}</p>
+          <p className='mb-2'><strong>Starttid:</strong> {auction.startTime}</p>
+          <p className='mb-2'><strong>Sluttid:</strong> {auction.endTime}</p>
         </div>
-        <div id='offer'></div>
-
       </div>
-    );
+      <div id='offer'></div>
+
+    </div>
+  );
 }
 export default AuctionDetails
