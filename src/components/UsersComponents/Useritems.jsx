e734e3d28f4311ef05df27e98e7041ffcc32735c
Barileva <veton@live.se>
2024-03-08 10:59:24 +0100

    test

--- src/components/UsersComponents/Useritems.jsx
+++ src/components/UsersComponents/Useritems.jsx
@@ -1,14 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 
-function Useritems() {
-    return <h1>User items veton</h1>
-
- 
-
-    function AuctionDetails({ auctionsList }) {
-        let { auctionId } = useParams();
-        let auction = auctionsList.find(auction => auction.id.toString() === auctionId);
-
-}
-export default Useritems
+export default UserItems;
