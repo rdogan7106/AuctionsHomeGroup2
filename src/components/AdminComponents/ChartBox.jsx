12d572c08e0f9b5cd440f756deaef54e3bbb3dd7
rdogan7106 <rdogan7106@gmail.com>
2024-03-08 20:49:06 +0100

    charts added

--- src/components/AdminComponents/ChartBox.jsx
+++ src/components/AdminComponents/ChartBox.jsx
@@ -1,176 +1,183 @@
-import { Box ,Typography} from "@mui/material";
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { Box, Typography } from "@mui/material";
 import PeopleOutlineOutlinedIcon from "@mui/icons-material/PeopleOutlineOutlined";
-import CategoryOutlinedIcon from '@mui/icons-material/CategoryOutlined';
-import CurrencyExchangeOutlinedIcon from '@mui/icons-material/CurrencyExchangeOutlined';
-import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
-import LocalOfferOutlinedIcon from '@mui/icons-material/LocalOfferOutlined';
+import CategoryOutlinedIcon from "@mui/icons-material/CategoryOutlined";
+import CurrencyExchangeOutlinedIcon from "@mui/icons-material/CurrencyExchangeOutlined";
+import AssignmentTurnedInIcon from "@mui/icons-material/AssignmentTurnedIn";
+import LocalOfferOutlinedIcon from "@mui/icons-material/LocalOfferOutlined";
 import { useAuth } from "../../context/Context.jsx";
+import React, { useEffect, useState } from "react";
 
-function ChartBox(){
-    const { auctionsList, userList } = useAuth();
-    let totalOffers = auctionsList.reduce((total, auction) => {
-        return total + auction.bids.length;
-    }, 0);
-  
-
-    return (
-        <div className="d-flex mt-3 ">
-        <div className="div  mx-3">
-          <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="center"
-            alignItems="center"
-            sx={{
-              width: 150,
-              height: 100,
-              borderRadius: 1,
-              bgcolor: "primary.main",
-              "&:hover": {
-                bgcolor: "primary.dark",
-              },
-            }}
-          >
-            <PeopleOutlineOutlinedIcon
-              sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
-            />
-            <Typography
-              variant="body1"
-              sx={{
-                color: "white",
-                fontSize: "1rem",
-              }}
-            >
-              {userList.length > 0 ? `${userList.length} User` : "No User"}
-            </Typography>
-          </Box>
-        </div>
-
-        <div className="div mx-3">
+function ChartBox({
+  totalOffers,
+  finishedAuctions,
+  inProgressAuctions,
+  users,
+}) {
+  const { auctionsList, userList } = useAuth();
+  return (
+    <div className="d-flex mt-3 ">
+      <div className="div  mx-3">
         <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="center"
-            alignItems="center"
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            width: 170,
+            height: 100,
+            borderRadius: 1,
+            bgcolor: "primary.main",
+            "&:hover": {
+              bgcolor: "primary.dark",
+            },
+          }}
+        >
+          <PeopleOutlineOutlinedIcon
+            sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
+          />
+          <Typography
+            variant="body1"
             sx={{
-              width: 150,
-              height: 100,
-              borderRadius: 1,
-              bgcolor: "success.main",
-              "&:hover": {
-                bgcolor: "success.dark",
-              },
+              color: "white",
+              fontSize: "1rem",
             }}
           >
-            <CategoryOutlinedIcon
-              sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
-            />
-            <Typography
-              variant="body1"
-              sx={{
-                color: "white",
-                fontSize: "1rem",
-              }}
-            >
-              {auctionsList.length > 0 ? `${auctionsList.length} Items` : "No Item"}
-            </Typography>
-          </Box>
-        </div>
-
-        <div className="div mx-3">
-        <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="center"
-            alignItems="center"
-            sx={{
-              width: 150,
-              height: 100,
-              borderRadius: 1,
-              bgcolor: "warning.main",
-              "&:hover": {
-                bgcolor: "warning.dark",
-              },
-            }}
-          >
-            <LocalOfferOutlinedIcon
-              sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
-            />
-            <Typography
-              variant="body1"
-              sx={{
-                color: "white",
-                fontSize: "1rem",
-              }}
-            >
-              {totalOffers.length > 0 ? `${totalOffers.length} Offers` : "No Offer"}
-            </Typography>
-          </Box>
-        </div>
-        <div className="div mx-3">
-        <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="center"
-            alignItems="center"
-            sx={{
-              width: 150,
-              height: 100,
-              borderRadius: 1,
-              bgcolor: "secondary.main",
-              "&:hover": {
-                bgcolor: "secondary.dark",
-              },
-            }}
-          >
-            <CurrencyExchangeOutlinedIcon
-              sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
-            />
-            <Typography
-              variant="body1"
-              sx={{
-                color: "white",
-                fontSize: "1rem",
-              }}
-            >
-              {totalOffers.length > 0 ? `${totalOffers.length} Offers` : "No Offer"}
-            </Typography>
-          </Box>
-
-        </div>
-
-        <div className="div mx-3">
-        <Box
-            display="flex"
-            flexDirection="row"
-            justifyContent="center"
-            alignItems="center"
-            sx={{
-              width: 150,
-              height: 100,
-              borderRadius: 1,
-              bgcolor: "error.main",
-              "&:hover": {
-                bgcolor: "error.dark",
-              },
-            }}
-          >
-            <AssignmentTurnedInIcon
-              sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
-            />
-            <Typography
-              variant="body1"
-              sx={{
-                color: "white",
-                fontSize: "1rem",
-              }}
-            >
-              {totalOffers.length > 0 ? `${totalOffers.length} Offers` : "No Offer"}
-            </Typography>
-          </Box>
-
-        </div>
+            {users.length > 0 ? `${users.length} User` : "No User"}
+          </Typography>
+        </Box>
       </div>
-    )
+
+      <div className="div mx-3">
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            width: 170,
+            height: 100,
+            borderRadius: 1,
+            bgcolor: "success.main",
+            "&:hover": {
+              bgcolor: "success.dark",
+            },
+          }}
+        >
+          <CategoryOutlinedIcon
+            sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
+          />
+          <Typography
+            variant="body1"
+            sx={{
+              color: "white",
+              fontSize: "1rem",
+            }}
+          >
+            {auctionsList.length > 0
+              ? `${auctionsList.length} Items`
+              : "No Item"}
+          </Typography>
+        </Box>
+      </div>
+
+      <div className="div mx-3">
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            width: 170,
+            height: 100,
+            borderRadius: 1,
+            bgcolor: "warning.main",
+            "&:hover": {
+              bgcolor: "warning.dark",
+            },
+          }}
+        >
+          <LocalOfferOutlinedIcon
+            sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
+          />
+          <Typography
+            variant="body1"
+            sx={{
+              color: "white",
+              fontSize: "1rem",
+            }}
+          >
+            {totalOffers > 0 ? `${totalOffers} Offers` : "No Offer"}
+          </Typography>
+        </Box>
+      </div>
+      <div className="div mx-3">
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            width: 170,
+            height: 100,
+            borderRadius: 1,
+            bgcolor: "secondary.main",
+            "&:hover": {
+              bgcolor: "secondary.dark",
+            },
+          }}
+        >
+          <CurrencyExchangeOutlinedIcon
+            sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
+          />
+          <Typography
+            variant="body1"
+            sx={{
+              color: "white",
+              fontSize: "1rem",
+            }}
+          >
+            {inProgressAuctions > 0
+              ? `${inProgressAuctions} In Progress`
+              : "No Progress"}
+          </Typography>
+        </Box>
+      </div>
+
+      <div className="div mx-3">
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            width: 170,
+            height: 100,
+            borderRadius: 1,
+            bgcolor: "error.main",
+            "&:hover": {
+              bgcolor: "error.dark",
+            },
+          }}
+        >
+          <AssignmentTurnedInIcon
+            sx={{ color: "white", width: 50, height: 50, marginRight: 1 }}
+          />
+          <Typography
+            variant="body1"
+            sx={{
+              color: "white",
+              fontSize: "1rem",
+            }}
+          >
+            {finishedAuctions > 0
+              ? `${finishedAuctions} Finished`
+              : "No Ending Auctions"}
+          </Typography>
+        </Box>
+      </div>
+    </div>
+  );
 }
-export default ChartBox
+export default ChartBox;
--- src/components/AdminComponents/Charts.jsx
+++ src/components/AdminComponents/Charts.jsx
@@ -1,15 +1,38 @@
 /* eslint-disable no-unused-vars */
 import { useAuth } from "../../context/Context.jsx";
 import ChartBox from "./ChartBox.jsx";
+import StatisticsChart from "./StatisticsCharts.jsx";
+import React, { useEffect, useState } from "react";
 
 
 function Charts() {
   const { auctionsList, userList } = useAuth();
+  const [totalOffers, setTotalOffers] = useState(0);
+  const [finishedAuctions, setFinishedAuctions] = useState(0);
+  const [inProgressAuctions, setInProgressAuctions] = useState(0);
+  const [users, setUsers] = useState(0)
+
+  useEffect(() => {
+    let newOffers = auctionsList.reduce(
+      (total, auction) => total + auction.bids.length,
+      0
+    );
+    let newFinished = auctionsList.filter(
+      (auction) => auction.status == "finished"
+    );
+    let userListUsers = userList.filter(user=> user.type=="user")
+    setUsers(userListUsers)
+    setTotalOffers(newOffers);
+    setFinishedAuctions(newFinished.length);
+    setInProgressAuctions(auctionsList.length - newFinished.length);
+    
+  }, [auctionsList, userList]);
 
   return (
     <div className="container ">
       <div className="row">
-    <ChartBox/>
+        <ChartBox totalOffers= {totalOffers} users = {users} finishedAuctions = {finishedAuctions}  inProgressAuctions = {inProgressAuctions}/>
+        <StatisticsChart />
       </div>
     </div>
   );
