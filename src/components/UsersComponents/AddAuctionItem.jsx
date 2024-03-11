f442bca1b94b682058ba9b5da4de68225d8d37a3
rdogan7106 <rdogan7106@gmail.com>
2024-03-11 13:32:01 +0100

    bug fixed

--- src/components/UsersComponents/AddAuctionItem.jsx
+++ src/components/UsersComponents/AddAuctionItem.jsx
@@ -89,11 +89,11 @@ function AddAuctionItem({ setActiveComponent, updateAuction }) {
               Price
             </label>
             <input
-              type="text"
+              type="number"
               className="form-control"
               id="price"
               name="price"
-              value={formData.itemDetails.price}
+              value={Number(formData.itemDetails.price)}
               onChange={handleChange}
               required
             />
