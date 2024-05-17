20d9af6138f46cd271b2e13d52c6c7d078150fdc
Barileva <veton@live.se>
2024-05-17 09:34:21 +0200

    t

--- cypress/specs/step_definitions/DeleteItems.js
+++ cypress/specs/step_definitions/DeleteItems.js
@@ -7,7 +7,8 @@ Given('I am logged in with my username and my passsword before deleting Items', 
 When('I have accessed on theDashboardlink in the navbar', () => {
   cy.wait(1000)
   cy.get("#navbarSupportedContent > ul > li:nth-child(2) > a").click()
-});
+}); 
+// t
 
 When('I click on the Auctions in the side menu in order to delete an auction', () => {
   cy.wait(1000)
